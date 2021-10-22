55fb8e66d6b7a2e877d61b78a8b9fceba4461f17
Elcoms <elcomsortsort@gmail.com>
2021-10-23 05:21:18 +0800

    Add cpp files

---
+++ Source/GAM300_Prototype/AdvancedSessionsClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class GAM300_PROTOTYPE_API AdvancedSessionsClass
+{
+public:
+	AdvancedSessionsClass();
+	~AdvancedSessionsClass();
+};
