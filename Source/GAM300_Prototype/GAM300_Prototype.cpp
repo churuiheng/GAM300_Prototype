76ca2d48e619d6da2597c14807ab45c9aa0e09a6
Elcoms <elcomsortsort@gmail.com>
2021-10-23 05:21:18 +0800

    Add cpp files

---
+++ Source/GAM300_Prototype/GAM300_Prototype.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GAM300_Prototype.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAM300_Prototype, "GAM300_Prototype" );
